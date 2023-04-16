9b0f92bed900336b78ae79e44b9f2c109aa61c73
MilaLopatina <123274488+MilaLopatina@users.noreply.github.com>
2023-04-16 19:01:15 +0200

    changed units

--- src/weather.js
+++ src/weather.js
@@ -42,7 +42,8 @@ export default function Weather() {
                     8
                   </span>
                   <span className="units">
-                    <p id="celsius">℃</p>|<p id="fahrenheit"></p>
+                    <p id="celsius">℃</p>
+                    <p id="fahrenheit"></p>
                   </span>
                 </div>
                 <div className="col-4 cloudy" id="cloudy">
