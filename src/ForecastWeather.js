92afa92115d8fe369a1eb0ccbb4ce8a2288a2860
MilaLopatina <123274488+MilaLopatina@users.noreply.github.com>
2023-04-29 23:00:56 +0200

    created weather forecast

---
+++ src/ForecastWeather.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import axios from "axios";
+import FormatDate from "./FormatDate";
+
+export default function forecastweather(props) {
+  let [loaded, setLoaded] = usestate(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function weatherResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=017d56650cd168d68067850318775d43&units=metric`;
+    axios.get(url).then(weatherResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="ForecastWeather">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <ForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
+}
